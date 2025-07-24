93f953edbbe80a3c6a8d82aad7d0ff290a89789d
Pietro Lira <pietro@outlook.com.br>
2025-07-24 17:36:50 -0300

    Add files via upload

---
+++ projeto02/Parte01/utils/train.py
@@ -0,0 +1,27 @@
+import torch
+from torch.nn import Module
+from torch.utils.data import DataLoader
+
+def train(model: Module, dataloader: DataLoader, criterion, optimizer, device):
+    model.train()
+    running_loss = 0.0
+    correct = 0
+    total = 0
+
+    for inputs, labels in dataloader:
+        inputs, labels = inputs.to(device), labels.to(device)
+
+        optimizer.zero_grad()
+        outputs = model(inputs)
+        loss = criterion(outputs, labels)
+        loss.backward()
+        optimizer.step()
+
+        running_loss += loss.item() * inputs.size(0)
+        _, predicted = outputs.max(1)
+        total += labels.size(0)
+        correct += predicted.eq(labels).sum().item()
+
+    epoch_loss = running_loss / total
+    accuracy = correct / total
+    return epoch_loss, accuracy
